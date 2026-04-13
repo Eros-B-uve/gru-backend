118e43b7c4f59d262c11d76a56cc928909ce5b0a
Eros-B-uve <eros.alber18@hotmail.com>
2026-04-12 22:09:44 -0600

    fix: configuracion puerto Render

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
     origin: '*', // Permitir solicitudes desde cualquier origen
     });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Servidor escuchando en el puerto ${port}`);
 }
 bootstrap();
