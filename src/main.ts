cc398a2563a999318127e759a43e466569bc61d6
Eros-B-uve <eros.alber18@hotmail.com>
2026-04-12 22:02:33 -0600

    fix: install pg driver

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import {ValidationPipe} from '@nestjs/common';
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
+    app.enableCors({
+    origin: '*', // Permitir solicitudes desde cualquier origen
     });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
